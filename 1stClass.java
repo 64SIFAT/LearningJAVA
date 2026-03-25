ab4b7392c2378b723ff623452c2bf98bdc9e1cf7
64SIFAT <sifat64persional@gmail.com>
2026-03-25 22:41:47 +0800

    1st commit

---
+++ 1stClass.java
@@ -0,0 +1,29 @@
+class FirstClass {
+  public static void main(String arg[]) {
+    //Output
+    System.out.println("\nHellow world with java\n");
+    System.out.println("Hello World");
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+    System.out.println();//sout and tab easy way to write full line
+
+    //Variables
+
+    String name = "Sifat Ullah";
+    int A = 34;
+    int B = 56;
+    double price = 34.56;
+
+    B = 25;
+    name = "Nadia";
+    
+
+
+
+
+
+  }
+}
